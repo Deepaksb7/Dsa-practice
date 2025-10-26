d3d6624a372de2123cddb5468b664a72c17a5619
Deepaksb7 <beharadeepak6@gmail.com>
2025-10-26 14:40:26 +0530

    bubble sort

---
+++ sorting.py
@@ -0,0 +1,140 @@
+# merge sort
+
+# def merge_sort(arr, left , right):
+
+#     if left < right:
+#         mid = (left + right)//2
+
+#         merge_sort(arr,left , mid)
+
+#         merge_sort(arr,mid + 1, right)
+
+#         merge(arr, left , mid ,right )
+
+# def merge(arr, left , mid , right):
+#     L = arr[left : mid+1]
+#     R = arr[mid+1 : right+1]
+
+#     i = j = 0
+#     k = left
+
+#     while i < len(L) and j < len(R):
+#         if L[i] <= R[j]:
+#             arr[k] = L[i] 
+#             i +=1
+#         else:
+#             arr[k] = R[j]
+#             j +=1
+#         k +=1
+
+#     while i < len(L):
+#         arr[k] = L[i]
+#         i+=1
+#         k+=1
+
+#     while j < len(R):
+#         arr[k] = R[j]
+#         j+=1
+#         k+=1
+
+
+
+# arr = [6, 3, 9, 5]
+# merge_sort(arr, 0, len(arr)-1)
+# print(arr)
+
+
+
+
+
+
+#quick sort
+
+# def quick_sort(arr, low , high):
+
+#     if low < high:
+#         pivit_point= partition(arr,low,high)
+
+#         quick_sort(arr,low,pivit_point - 1)
+
+#         quick_sort(arr,pivit_point + 1,high)
+
+# def partition(arr,low,high):
+#     pivit = arr[high]
+#     i= low - 1
+#     for j in range(low , high):
+#         if arr[j] < pivit:
+#             i+=1
+#             arr[i],arr[j]= arr[j],arr[i]
+
+#     arr[i+1],arr[high] = arr[high],arr[i+1]
+#     return i + 1
+
+
+# arr = [6, 3, 9, 5, 2, 8]
+
+# quick_sort(arr,0,5)
+
+# print(arr)
+
+
+
+
+
+
+
+
+#heap sort
+
+# def heapify(arr,n,i):
+#     largest = i
+#     left = 2*i+1
+#     right = 2*i+1
+
+#     if n > left and arr[left] > arr[largest]:
+#         largest = left
+
+#     if n > right and arr[right] > arr[largest]:
+#         largest = right
+
+#     if largest != i:
+#         arr[i],arr[largest] = arr[largest],arr[i]
+#         heapify(arr,n,largest)
+
+
+# def heap_sort(arr):
+#     n = len(arr)
+
+#     for i in range(n//2-1,-1,-1):
+#         heapify(arr,n,i)
+
+#     for i in range(n-1,0,-1):
+#         arr[i],arr[0] = arr[0],arr[i]
+#         heapify(arr,i,0)
+    
+#     return arr
+
+
+# arr = [4, 10, 3, 5, 1]
+# print(heap_sort(arr))
+
+
+
+
+
+#Bubble sort 
+
+# def bubbleSort(arr):
+#     for i in range(len(arr)-1):
+#         for j in range(len(arr)-i-1):
+#             if arr[j] > arr[j+1]:
+#                 arr[j],arr[j+1] = arr[j+1],arr[j]
+#     return arr
+
+
+# arr = [4, 10, 3, 5, 1]
+# print(bubbleSort(arr))
+
+
+
+
