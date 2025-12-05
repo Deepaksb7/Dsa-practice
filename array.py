b0fd9d935ee48e9bf07bafbe10b859f9af36e4a3
Deepaksb7 <beharadeepak6@gmail.com>
2025-12-05 20:20:40 +0530

    new solved

--- array.py
+++ array.py
@@ -115,18 +115,131 @@
 #  non-zero elements.
 
 
-def zeroRight(arr):
-    pos = 0
+# def zeroRight(arr):
+#     pos = 0
 
-    for i in range(len(arr)):
-        if arr[i] != 0:
-            arr[pos] = arr[i]
-            pos += 1
+#     for i in range(len(arr)):
+#         if arr[i] != 0:
+#             arr[pos] = arr[i]
+#             pos += 1
         
-    for i in range(pos,len(arr)):
-        arr[i] = 0
+#     for i in range(pos,len(arr)):
+#         arr[i] = 0
 
-    return arr
+#     return arr
 
-arr = [0, 1, 0, 3, 12]
-print(zeroRight(arr))
+# arr = [0, 1, 0, 3, 12]
+# print(zeroRight(arr))
+
+
+
+#  6. Given an array of integers, return the length of the longest sequence of consecutive
+#  integers (not necessarily contiguous in the array).
+
+# def sequence(arr):
+#     map = {}
+#     longest = 0
+#     for i in arr:
+#         map[i] = i - 1
+
+#     for i in range(len(arr)):
+#         if arr[i] - 1 not in map:
+#             current = arr[i]
+#             count = 1
+
+#             while current + 1 in map:
+#                 current = current + 1
+#                 count += 1       
+
+#             longest= max(longest,count)
+#     return longest
+
+# arr = [100, 4, 200, 1, 3, 2]
+# print(sequence(arr))
+
+
+# def sequence(arr):
+#     numSet = set(arr)
+#     longest = 0
+
+#     for n in numSet:
+#         if n - 1 not in numSet:
+#             length = 1
+#             current = n
+#             while current +1 in numSet:
+#                 current += 1
+#                 length += 1
+#             longest = max(longest , length)
+
+#     return longest
+
+# arr = [100, 4, 200, 1, 3, 2 ,5 ,6,7,8]
+# print(sequence(arr))
+
+
+
+
+#  7. Given an array of integers, find the element that appears more than n/2 times (you may
+#  assume such an element always exists).
+
+# def count(arr):
+#         candidate = 0
+#         counter = 0
+#         for num in arr:
+#             if counter == 0:
+#                   candidate = num
+#             if num == candidate:
+#                   counter += 1
+#             else:
+#                   counter -= 1
+#         return candidate
+
+
+# arr = [3, 3, 4, 3, 2, 3, 3]
+# print(count(arr))
+
+
+# 8. Given an array of strings, group the strings that are anagrams of each other.
+
+# def anagrams(arr):
+#     map = {}
+#     for s in arr:
+#         key ="".join(sorted(s))
+#         if key not in map:
+#             map[key] = []
+#         map[key].append(s)              
+#     return list(map.values())
+
+# arr=["eat", "tea", "tan", "ate", "nat", "bat"]
+# print(anagrams(arr))
+
+
+#  9. Given an array of integers, find all unique triplets [a,b,c] such that a + b + c = 0.
+
+
+def threeSum(arr):
+    arr.sorted()
+    result = []
+    for i in range(len(arr)):
+        if i > 0 and arr[i] == arr[i-1]:
+            continue
+        left = i + 1
+        right = len(arr) -1 
+        while left < right:
+            total = arr[i] + arr[left] + arr[right]
+            if total == 0:
+                result.append([arr[i], arr[left], arr[right]])
+                left += 1
+                right -= 1
+                while left < right and arr[left] == arr[left - 1]:
+                    left +=1
+                while left < right and arr[right] == arr[right + 1]:
+                    right -= 1
+
+            elif total < 0:
+                left += 1  
+            else:
+                right -= 1 
+
+arr= [-1, 0, 1, 2, -1, -4]
+print(threeSum(arr))
